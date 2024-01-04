f444ce1c7a7fdb288fd663ee53cb2b341b201e85
ProGrammer_365 <133169817+ProCoder36908@users.noreply.github.com>
2024-01-04 18:27:04 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare.benrogo.net/',
+    bare: 'https://nebulaproxy.io/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
